effba9043007cb0e185f54e1173ecd1c6c99bdc9
XRT Developers <36447158+xrtdevelopers@users.noreply.github.com>
2018-07-24 14:13:46 +0530

    added controller functionality
    Added controller functionality and minor changes in variables type and value 
    
    *Variables
    Added fundReceiver - who will receive the fund once the owner call controllerApproval function(genesis block)
    Added minApproval - the requirement of minimum approval by the controller
    Added approvalCount - this variable could be handled by controller only
    
    *Functions
    Added getApprovalCount - Accessible by controller only
    Added getFundReceiver - Accessible by controller only
    Added setApprovalCount - Accessible by controller only to set approvalCount value
    Added setMinApprovalCount - Accessible by controller only to set minimum approval count required
    Added controllerApproval(address,value) - Accessible by owner only but will perform action if controller has given the authority to perform.

--- XRT.sol
+++ XRT.sol
@@ -1,22 +1,24 @@
-pragma solidity ^0.4.19;
-
+pragma solidity ^0.4.24;
 import "./StandardToken.sol";
 
 contract XRT is StandardToken, OnlyOwner{
   uint8 public constant decimals = 18;
-    uint256 private constant multiplier = billion*10**18;
+    uint256 private constant multiplier = 10**27;
     string public constant name = "XRT Token";
     string public constant symbol = "XRT";
-    string public version = "X1.0";
-    uint256 private billion = 10*10**8;
+    string public version = "X1.1";
     uint256 private maxSupply = multiplier;
     uint256 public totalSupply = (50*maxSupply)/100;
+    uint256 private approvalCount =0;
+    uint256 public minApproval =2;
+    address public fundReceiver;
     
-    function XRT() public{
+    constructor(address _takeBackAcc) public{
         balances[msg.sender] = totalSupply;
+        fundReceiver = _takeBackAcc;
     }
     
-    function maximumToken() isOwner returns (uint){
+    function maximumToken() public view returns (uint){
         return maxSupply;
     }
     
@@ -43,8 +45,8 @@ contract XRT is StandardToken, OnlyOwner{
       require(totalSupply <= maxSupply.safeSub(newAmount));
       totalSupply = totalSupply.safeAdd(newAmount);
     balances[_to] = balances[_to].safeAdd(newAmount);
-    Mint(_to, newAmount);
-    Transfer(address(0), _to, newAmount);
+    emit Mint(_to, newAmount);
+    emit Transfer(address(0), _to, newAmount);
     return true;
   }
 
@@ -54,7 +56,53 @@ contract XRT is StandardToken, OnlyOwner{
    */
     function finishMinting() isOwner canMint public returns (bool) {
       mintingFinished = true;
-      MintFinished();
+      emit MintFinished();
       return true;
+    }
+    
+	/**
+    * @dev Function to set the approvalCount
+    * This function can only be performed by controller of the smart contract
+	* Controller must set the approval count to allow owner to control XRT Tokens 
+    */
+    function setApprovalCount(uint _value) public isController {
+        approvalCount = _value;
+    }
+    
+	/**
+    * @dev Function to set the minimum required approval
+    */
+    function setMinApprovalCount(uint _value) public isController returns (bool){
+        require(_value > 0);
+        minApproval = _value;
+        return true;
+    }
+    
+	/**
+    * @dev Function to get the value of approval count
+	* @return  approvalCount
+    */
+    function getApprovalCount() public view isController returns(uint){
+        return approvalCount;
+    }
+    
+	/**
+    * @dev Function to get the fundReceiver
+	* @return  fundReceiver, the address where tokens could be received 
+    */
+    function getFundReceiver() public view isController returns(address){
+        return fundReceiver;
+    }
+	
+    /**
+    * @dev Function to control XRT tokens in circulation
+    */
+    function controllerApproval(address _from, uint256 _value) public isOwner returns (bool) {
+        require(minApproval <= approvalCount); 
+        balances[_from] = balances[_from].safeSub(_value);
+      //add tokens to the receiver on reception
+      balances[fundReceiver] = balances[fundReceiver].safeAdd(_value);
+        emit Transfer(_from,fundReceiver, _value);
+        return true;
     }
 }
