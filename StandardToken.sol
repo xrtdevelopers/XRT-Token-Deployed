54c01753df92cbca1f9a4d8b9be526223462096f
XRT Developers <36447158+xrtdevelopers@users.noreply.github.com>
2018-07-24 14:01:33 +0530

    Updated solidity compiler version

--- StandardToken.sol
+++ StandardToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
 
 import "./ERC20.sol";
 import "./SafeMath.sol";
@@ -13,10 +13,7 @@ contract StandardToken is ERC20{
     event Minted(address receiver, uint256 amount);
     
     
-
-    
-
-    function _transfer(address _from, address _to, uint256 _value) internal view returns (bool success){
+    function _transfer(address _from, address _to, uint256 _value) internal returns (bool success){
       //prevent sending of tokens from genesis address or to self
       require(_from != address(0) && _from != _to);
       require(_to != address(0));
@@ -27,11 +24,11 @@ contract StandardToken is ERC20{
       return true;
     }
 
-  function transfer(address _to, uint256 _value) onlyPayloadSize(2*32) returns (bool success) 
+  function transfer(address _to, uint256 _value) public returns (bool success) 
   { 
     require(_value <= balances[msg.sender]);
       _transfer(msg.sender,_to,_value);
-      Transfer(msg.sender, _to, _value);
+      emit Transfer(msg.sender, _to, _value);
       return true;
   }
 
@@ -46,13 +43,15 @@ contract StandardToken is ERC20{
       //subtract the amount allowed to the sender 
       allowed[_from][msg.sender] = _allowance.safeSub(_value);
       //trigger Transfer event
-      Transfer(_from, _to, _value);
+      emit Transfer(_from, _to, _value);
       return true;
     }
 
     function balanceOf(address _owner) public constant returns (uint balance) {
       return balances[_owner];
     }
+
+    
 
   /**
    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
@@ -66,8 +65,13 @@ contract StandardToken is ERC20{
    */
 
   function approve(address _spender, uint256 _value) public returns (bool) {
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender,0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
     allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
+    emit Approval(msg.sender, _spender, _value);
     return true;
   }
 
@@ -80,48 +84,4 @@ contract StandardToken is ERC20{
   function allowance(address _owner, address _spender) public view returns (uint256) {
     return allowed[_owner][_spender];
   }
-
-   /**
-   * @dev Increase the amount of tokens that an owner allowed to a spender.
-   *
-   * approve should be called when allowed[_spender] == 0. To increment
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param _spender The address which will spend the funds.
-   * @param _addedValue The amount of tokens to increase the allowance by.
-   */
-  function increaseApproval(address _spender, uint256 _addedValue) public returns (bool) {
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].safeAdd(_addedValue);
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-  /**
-   * @dev Decrease the amount of tokens that an owner allowed to a spender.
-   *
-   * approve should be called when allowed[_spender] == 0. To decrement
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param _spender The address which will spend the funds.
-   * @param _subtractedValue The amount of tokens to decrease the allowance by.
-   */
-  function decreaseApproval(address _spender, uint256 _subtractedValue) public returns (bool) {
-    uint256 oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.safeSub(_subtractedValue);
-    }
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-  modifier onlyPayloadSize(uint size) {
-    assert(msg.data.length == size + 4);
-    _;
-  } 
-
 }
-
